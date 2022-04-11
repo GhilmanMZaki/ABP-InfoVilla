a73a586d5f821bda7a94642eaa228b8d4b0ffc6c
GhilmanMZaki <ghilman.zaki1@gmail.com>
2022-04-11 16:13:44 +0700

    update dashboard frontend

--- info-villa/routes/web.php
+++ info-villa/routes/web.php
@@ -13,12 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/login', function () {
     return view('login');
 });
 Route::get('/signup', function () {
     return view('signup');
 });
-Route::get('/home', function () {
+Route::get('/', function () {
     return view('home');
 });

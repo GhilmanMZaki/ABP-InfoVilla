d3d55d1820c991041016630b7d7461e2b072d99b
GhilmanMZaki <ghilman.zaki1@gmail.com>
2022-04-11 21:29:41 +0700

    update dashboard front end

--- info-villa/resources/views/home.blade.php
+++ info-villa/resources/views/home.blade.php
@@ -9,6 +9,7 @@
 
 <!-- Bootstrap CSS v5.0.2 -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+<link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <!-- Bootstrap JavaScript Libraries -->
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -19,111 +20,148 @@
   background-position: center;
 }
 .card:hover{
-  background-color: green;
+  background :linear-gradient(0deg, rgba(0,0,0,1) 0%, rgba(9,126,8,1) 100%);
 }
 .card:hover .card-body h4{
   color: aliceblue;
-  transition: 1s
+  transition: 0.5s
 }
 .card:hover .card-body p{
   color: aliceblue;
-  transition: 1s
+  transition: 0.5s
 }
-.card-fasilitas{
-  right: 0;
-  left: 0;
-  background-color: aliceblue;
+
+.pb-2{
+  padding-top: 5px;
+  font-size: 18px;
+}
+.pb-2 .p{
+  font-size: 14px;
 }
 </style>
 </head>
 <body>
+    <!--Navbar -->  
     <nav class="navbar navbar-expand-lg navbar-dark bg-success shadow p-3">
-        <div class="container">
-          <a class="navbar-brand h2" href="#"><i class="bi bi-house-fill"></i> INFOVILLA</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav ms-auto">
-                <a class="nav-link mx-2" href="#"><i class="bi bi-hand-index-thumb"></i> Terserah!</i></a>
-                <a class="nav-link ms-auto" href="/login">Login</a>
-          </div>
-        </div>
-      </nav>
-      <div class="container-fluid p-xl-5 shadow p-3" id="search">
-        <div class="container">
-          <div class="row">
-            <div class="col-2 offset-1 text-center py-3 bg-dark">
-              <h4 class="text-light align-self-center">Cari Lokasi Villa</h4>
-            </div>
-            <div class="col-7 md-auto py-3 bg-dark">
-              <form class="d-flex" action="/" method="">
-                <input class="form-control me-2" name="searchLokasi" type="search" placeholder="Lokasi Villa" aria-label="Search">
-                <button class="btn btn-success w-25" type="submit">Search <i class="bi bi-search"></i></button>
-              </form>
-            </div>
-          </div>
-        </div>    
-      </div>
-
-      <div class="container shadow-lg">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-12 text-center py-5">
-              <h1 class="text-success">Rekomendasi Villa Untuk Kamu</h1>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-4">
-              <div class="card p-2" style="width: 22rem">
-                <img src="{{url('/storage/img/villa-hira-pangalengan.png')}}" alt="villa">
-                <div class="card-body">
-                  <h4>Villa Hira Pangalengan</h4>
-                  <p>Jl. Situ Cileunca, Pulosari </br>0821-2837-6776</p>
-                </div>
-                <div class="card-fasilitas">
-                  <span>
-                    <i class="bi bi-door-closed-fill"></i> 2
-                    <p>Kamar Tidur</p>
-                  </span>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-4">
-              <div class="card p-2" style="width: 22rem">
-                <img src="{{url('/storage/img/villa-hira-pangalengan.png')}}" alt="villa">
-                <div class="card-body">
-                  <h4>Villa Hira Pangalengan</h4>
-                  <p>Jl. Situ Cileunca, Pulosari </br>0821-2837-6776</p>
-                </div>
-                <div class="card-fasilitas">
-                  <span>
-                    <i class="bi bi-door-closed-fill"></i> 2
-                    <p>Kamar Tidur</p>
-                  </span>
-                </div>
-              </div>
-            </div>
-
-            <div class="col-4">
-              <div class="card p-2" style="width: 22rem">
-                <img src="{{url('/storage/img/villa-hira-pangalengan.png')}}" alt="villa">
-                <div class="card-body">
-                  <h4>Villa Hira Pangalengan</h4>
-                  <p>Jl. Situ Cileunca, Pulosari </br>0821-2837-6776</p>
-                </div>
-                <div class="card-fasilitas">
-                  <span>
-                    <i class="bi bi-door-closed-fill"></i> 2
-                    <p>Kamar Tidur</p>
-                  </span>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div class="container">
+        <a class="navbar-brand h2" href="#"><i class="bi bi-house-fill"></i> INFOVILLA</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav ms-auto">
+              <a class="nav-link mx-2" href="#"><i class="bi bi-hand-index-thumb"></i> Terserah!</i></a>
+              <a class="nav-link ms-auto" href="/login">Login</a>
         </div>
       </div>
+    </nav>
+
+    <!--Kolom Search -->  
+    <div class="container-fluid p-xl-5 shadow p-3" id="search">
+      <div class="container">
+        <div class="row">
+          <div class="col-2 offset-1 text-center py-3 bg-dark">
+            <h4 class="text-light align-self-center">Cari Lokasi Villa</h4>
+          </div>
+          <div class="col-7 md-auto py-3 bg-dark">
+            <form class="d-flex" action="/" method="">
+              <input class="form-control me-2" name="searchLokasi" type="search" placeholder="Lokasi Villa" aria-label="Search">
+              <button class="btn btn-success w-25" type="submit">Search <i class="bi bi-search"></i></button>
+            </form>
+          </div>
+        </div>
+      </div>    
+    </div>
+
+    <!--Card Rekomendasi -->  
+    <div class="container pb-xl-4 shadow-lg">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-12 text-center py-5">
+            <h1 class="text-success">Rekomendasi Villa Untuk Kamu</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-4 mb-4">
+            <div class="card p-2" style="width: 22rem">
+              <img src="{{url('/storage/img/villa-hira-pangalengan.png')}}" alt="villa">
+              <div class="card-body">
+                <h4>Villa Hira Pangalengan</h4>
+                <p>Jl. Situ Cileunca, Pulosari </br>0821-2837-6776</p>
+              </div>
+              <div class="card-fasilitas d-flex justify-content-between bg-light">
+                <span class="pt-3">
+                  <i class="ri-hotel-bed-fill ri-xl"></i> 2
+                  <p>Kamar Tidur</p>
+                </span>
+
+                <span class="pt-3">
+                  <i class="ri-wifi-fill ri-xl"></i>
+                  <p>Free Wifi</p>
+                </span>
+
+                <span class="pt-3">
+                  <i class="ri-home-5-fill ri-xl"></i> 14
+                  <p>Kapasitas orang</p>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-4 mb-4">
+            <div class="card p-2" style="width: 22rem">
+              <img src="{{url('/storage/img/villa-hira-pangalengan.png')}}" alt="villa">
+              <div class="card-body">
+                <h4>Villa Hira Pangalengan</h4>
+                <p>Jl. Situ Cileunca, Pulosari </br>0821-2837-6776</p>
+              </div>
+              <div class="card-fasilitas d-flex justify-content-between bg-light">
+                <span class="pt-3">
+                  <i class="ri-hotel-bed-fill ri-xl"></i> 2
+                  <p>Kamar Tidur</p>
+                </span>
+
+                <span class="pt-3">
+                  <i class="ri-wifi-fill ri-xl"></i>
+                  <p>Free Wifi</p>
+                </span>
+
+                <span class="pt-3">
+                  <i class="ri-home-5-fill ri-xl"></i> 14
+                  <p>Kapasitas orang</p>
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-4 mb-4">
+            <div class="card p-2" style="width: 22rem">
+              <img src="{{url('/storage/img/villa-hira-pangalengan.png')}}" alt="villa">
+              <div class="card-body">
+                <h4>Villa Hira Pangalengan</h4>
+                <p>Jl. Situ Cileunca, Pulosari </br>0821-2837-6776</p>
+              </div>
+              <div class="card-fasilitas d-flex justify-content-between bg-light">
+                <span class="pt-3">
+                  <i class="ri-hotel-bed-fill ri-xl"></i> 2
+                  <p>Kamar Tidur</p>
+                </span>
+
+                <span class="pt-3">
+                  <i class="ri-wifi-fill ri-xl"></i>
+                  <p>Free Wifi</p>
+                </span>
+
+                <span class="pt-3">
+                  <i class="ri-home-5-fill ri-xl"></i> 14
+                  <p>Kapasitas orang</p>
+                </span>
+              </div>
+            </div>
+          </div>     
+        </div>
+      </div>
+    </div>
 
 
 
